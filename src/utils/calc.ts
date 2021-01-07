cb234bedb302e584f7b97d245ca3dc26c008acf2
Nadno <eunadno@gmail.com>
2021-01-07 12:26:46 -0300

    util calc functions

---
+++ src/utils/calc.ts
@@ -0,0 +1,4 @@
+export const sum = (first: number, second: number) =>  first + second;
+export const subtract = (first: number, second: number) =>  first - second;
+export const multiply = (first: number, second: number) =>  first * second;
+export const divide = (first: number, second: number) =>  first / second;
